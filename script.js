22f228dc6ab72e3135a9395feab668a730148191
Nagarjun <72065721+nagarjunpr44@users.noreply.github.com>
2022-01-04 15:09:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+const random_dog=document.getElementById('random_dog');
+const dog_button=document.getElementById('dog_button');
+
+function getRandomDog() {
+	try{
+
+		fetch('https://random.dog/woof.json')
+		.then(res => res.json())
+		.then(data => {
+			if(data.url.includes('.mp4')) {
+				getRandomDog();
+			}
+			else {
+				random_dog.innerHTML = `<img src=${data.url} alt="dog"/>`;
+			}
+		});
+	}
+		catch(e)
+		{
+			console.log(e);
+		}
+		
+}
+dog_button.addEventListener('click',getRandomDog)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
